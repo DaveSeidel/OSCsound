51ef5b381e10be6c6a59a0f5dbbc72c55911f178
DaveSeidel <dave.seidel@gmail.com>
2017-02-13 17:43:04 -0500

    Refactored

--- OSCsound.py
+++ OSCsound.py
@@ -1,120 +1,39 @@
-# OSCsound - Version 0.1
-# Author: Steven Yi
+#
+# OSCsound - Version 0.2
+# Author: Steven Yi, Dave Seidel
 #
 # Script for rapid live Csound application development with OSC
-# 
+#
 # Licensed under LGPL. Please view LICENSE for more information.
 #
 
-from csnd6 import Csound, CsoundPerformanceThread, csoundInitialize
-from OSC import OSCServer
-from time import sleep
 import argparse
-import types
+import sys
 
-# setup csound
-csoundInitialize(3)
-csound = None
-csPerfThread = None
-
-def csound_start(csd_file):
-    global csound 
-    global csPerfThread
-
-    csound = Csound()
-    csound.SetOption("-odac")  
-    csound.Compile(csd_file)     
-    csound.Start()  
-    csPerfThread = CsoundPerformanceThread(csound)
-    csPerfThread.Play()
-
-def csound_stop():
-    global csPerfThread
-    global csound
-    csPerfThread.Stop()
-    csPerfThread.Join()
-    csound.Stop()
-
-###
-
-server = None
-run = True
-
-def handle_timeout(self):
-    self.timed_out = True
-
-def handle_score(path, tags, args, source):
-    global csPerfThread
-    #print "TESTING", args[0]
-    csPerfThread.InputMessage(args[0])
-
-def handle_cc(path, tags, args, source):
-    global csound
-    print path
-    if(len(args) != 2):
-        print "Error: CC must have two args: channelName floatValue"
-        return
-    print "TESTING: %s %s"%(args[0], args[1])
-    csound.SetChannel(args[0], args[1])
-
-def default_callback(path, tags, args, source):
-    global csound
-    if path.startswith("/cc/"):
-        csound.SetChannel(path[4:], args[0])
-    else:
-        print "ignoring message :", path, tags, args
-
-def quit_callback(path, tags, args, source):
-    global run
-    run = False
-    print "Quitting Server..."
+from CsoundProxy import CsoundProxy
+from OSCServerProxy import OSCServerProxy
 
 
-def each_frame():
-    global server
-    server.timed_out = False
-    while not server.timed_out:
-        server.handle_request()
+def process_args():
+    parser = argparse.ArgumentParser(prog='OSCsound',
+                                     description='OSCsound - version 0.2-ds')
 
+    parser.add_argument("csd_file", help="Csound CSD file to run")
+    parser.add_argument("-p", "--port", help="server port (default 7110)",
+                        type=int, default=7110)
 
-def server_start(port=7110):
-    global server
-    server = OSCServer ( ("localhost", 7110))
-    server.timeout = 0
-
-    server.handle_timeout = types.MethodType(handle_timeout, server)
-
-    server.addMsgHandler("/sco", handle_score)
-    server.addMsgHandler("/cc", handle_cc)
-    server.addMsgHandler("/quit", quit_callback)
-    server.addMsgHandler("default", default_callback)
-
-def server_stop():
-    global server 
-    server.close()
-
-
+    return parser.parse_args()
 
 
 def main():
-    
-    parser = argparse.ArgumentParser(prog='OSCsound', 
-                                     description='OSCsound - version 0.1')
+    args = process_args()
 
-    parser.add_argument("csd_file", help="Csound CSD File to run")
-    parser.add_argument("-p", "--port", help="Server Port, defaults to 7110", type=int, default=7110)
+    with CsoundProxy(args.csd_file) as csound:
+        with OSCServerProxy(csound, args.port) as osc:
+            osc.run()
 
-    args = parser.parse_args()
+    return 0
 
-    csound_start(args.csd_file)
-    server_start(args.port)
-
-    while run:
-        sleep(1)
-        each_frame()
-
-    csound_stop()
-    server_stop()
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
--- test.csd
+++ test.csd
@@ -6,19 +6,24 @@ ksmps=32
 nchnls=2
 0dbfs=1
 
-instr 1 
+instr Ladder
+  kfreq   chnget "freq"
+  kcutoff chnget "cutoff"
+  kres    chnget "resonance"
 
-kcutoff chnget "cutoff"
-kres    chnget "resonance"
+  kcutoff port kcutoff, 0.05
+  kres port kres, 0.05
 
-kcutoff port kcutoff, 0.05
-kres port kres, 0.05
-
-aout vco2 0.5, 440
-aout moogladder aout, kcutoff, kres
-outs aout, aout
+  aout1 vco2 0.5, kfreq
+  aout2 moogladder aout1, kcutoff, kres
+  outs aout1, aout2
 endin
 
+alwayson "Ladder"
+
 </CsInstruments>
+
+<CsScore>
+</CsScore>
 
 </CsoundSynthesizer>
